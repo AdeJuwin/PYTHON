fc4e1d7cc266383792b6c9fac429a7787659936f
AdeJuwin <olajuwonsunday01@gmail.com>
2022-05-26 20:58:35 +0100

    New task 1

---
+++ task1.py
@@ -0,0 +1,32 @@
+#Arithemic operations
+#Varibles x, y to store input integers data
+#varible z to store result of operation
+
+x = 20
+y= 10
+
+#Additional Operation
+print("User perform Addittion operation")
+z = x+y
+print("Answer to the operation is")
+print(z)
+
+
+#Substraction Operation
+print("User perform Substraction operation")
+z = x-y
+print("Answer to the operation is")
+print(z)
+
+#Multiplication Operation
+print("User perform Multiplication operation")
+z = x*y
+print("Answer to the operation is")
+print(z)
+
+#Division Operation
+print("User perform Divisional operation")
+z = x/y
+print("Answer to the operation is")
+print(z)
+
---
+++ uptask1.py
@@ -0,0 +1,32 @@
+def multiple_number():
+    a = int(input("kindly enter an integer:"))
+    b = int(input("kindly enter an integer:"))
+    return a*b
+
+def add_number():
+    a = int(input("kindly enter an integer:"))
+    b = int(input("kindly enter an integer:"))
+    return a+b
+
+def division_number():
+    a = int(input("kindly enter an integer:"))
+    b = int(input("kindly enter an integer:"))
+    return a/b
+
+def sub_number():
+        a = int(input("kindly enter an integer:"))
+        b = int(input("kindly enter an integer:"))
+        return a * b
+
+user = input("Enter Operation (*,-,+,/):")
+result ="Operation not Valid"
+
+if user == "*":
+    result = multiple_number()
+elif user == "-":
+    result = sub_number()
+elif user == "/":
+    result = division_number()
+elif user == "+":
+    result = add_number()
+print(result)
